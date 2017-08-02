8de8ef77b4b3b834faa0dc2d28c0cc6a537a37c1
Elsa Andrea Garnica Reyes <elsaandreagarnicareyes@MacBook-Air-de-Elsa.local>
2017-08-02 00:09:08 -0500

    Avance

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
-var sass = require("gulp-sass");
-
+var sass = require('gulp-sass');
 gulp.task('jade', function(){
     gulp.src('./src/*.jade')
     .pipe(jade({
         pretty: true
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+});
+gulp.task('sass', function() {
+  gulp.src('./src/*.sass')
+    .pipe(sass({
+        pretty:true
+     }))
+    .pipe(gulp.dest('./dist/css'));
 });
 gulp.task('watch', function(){
-    gulp.watch('./src/*.jade', ['jade']);
+    gulp.watch('./src/*.jade', ['jade'])
+    gulp.watch('./src/*.sass', ['sass'])
 });
-
-gulp.task('default', ['jade', 'watch']);
+gulp.task('default', ['sass', 'jade', 'watch']);
